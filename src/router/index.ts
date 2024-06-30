694f702482bceb772a3b74be14f8f19c0dd55c6e
The_Law <gabriel.griffin@epitech.eu>
2024-06-30 21:41:02 +0200

    🚀 Reverting to simple web history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import indexVue from "@/pages/index.vue";
 import defaultLayout from "@/layouts/default.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
